0f02e9b6992462d849c94c61949d5f60500f275d
jwright <jwright@4549f004-a2ad-11e4-8197-c1af43012850>
2015-02-02 03:16:52 +0000

    Added the partially completed Linux install script to trunk.
    git-svn-id: https://opendesignengine.net/svn/mtk/trunk@2 4549f004-a2ad-11e4-8197-c1af43012850

---
+++ install/linux/mtk_setup_script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Author: Mach 30: Foundation for Space Development - http://mach30.org
+# License: Apache 2.0
+
+# Writen and tested on Ubuntu 14.04
+# Must be run with superuser priveleges
+
+apt-get update
+
+#Kile IDE
+apt-get install -y kile
+
+#Sage
+apt-add-repository -y ppa:aims/sagemath
+apt-get update
+apt-get install -y sagemath-upstream-binary
+
+#Configure sagetex to work properly with sage
+cp -R /usr/lib/sagemath/local/share/texmf/tex/ `kpsewhich -var-value=TEXMFLOCAL`
+texhash `kpsewhich -var-value=TEXMFLOCAL`
