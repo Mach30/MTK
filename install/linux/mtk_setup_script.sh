cd65ff86a6819d0b3b01791524e4942c0a4dc4b3
jwright <jwright@4549f004-a2ad-11e4-8197-c1af43012850>
2015-02-20 02:45:08 +0000

    Reworked the script to be less verbose and to insert the shell escape option for the PDFLaTeX build tool.
    git-svn-id: https://opendesignengine.net/svn/mtk/trunk@9 4549f004-a2ad-11e4-8197-c1af43012850

--- install/linux/mtk_setup_script.sh
+++ install/linux/mtk_setup_script.sh
@@ -5,30 +5,37 @@
 # Writen and tested on Ubuntu 14.04
 # Must be run with superuser priveleges
 
-apt-get update
+echo "Running an initial update..."
+apt-get update > /dev/null
 
+echo "Installing the Kile LaTex IDE..."
 #Kile IDE
-apt-get install -y kile
+apt-get install -y kile  > /dev/null
 
+echo "Installing Sage..."
 #Sage
-apt-add-repository -y ppa:aims/sagemath
-apt-get update
-apt-get install -y sagemath-upstream-binary
+apt-add-repository -y ppa:aims/sagemath  > /dev/null
+apt-get update  > /dev/null
+apt-get install -y sagemath-upstream-binary  > /dev/null
 
+echo "Configuring SageTex to work properly with Sage..."
 #Configure sagetex to work properly with sage
 cp -R /usr/lib/sagemath/local/share/texmf/tex/ `kpsewhich -var-value=TEXMFLOCAL`
 texhash `kpsewhich -var-value=TEXMFLOCAL`
 
+echo "Installing LaTex extras package..."
 #Will get an error about makecmds.sty missing without this
-apt-get install -y texlive-latex-extra
+apt-get install -y texlive-latex-extra  > /dev/null
 
+echo "Installing Pygments package..."
 #Pygments for Python syntax highlighting
-sudo apt-get install python-pygments
+sudo apt-get install python-pygments  > /dev/null
 
+echo "Automating MTK Sage steps in Kile IDE..."
 # Do Kile shell escape automatically
-# Insert -shell-escape at the head of options line under [Tool/PDFLaTeX/Default]
-# ~/.kde/share/config/kilerc
-# Search home dir for kilerc and then use awk/sed to edit file in-place
+KILERC_LOCATION="$(find /home/$(who am i | awk '{print $1}') -name kilerc)"
+sed '/^options=-interaction=nonstopmode/ s/^\(options=\)/\1-shell-escape /' $KILERC_LOCATION
 
+echo "Adding Pint for units handling..."
 # Add Pint for units handling
-sage --python -m easy_install pint
+sage --python -m easy_install pint  > /dev/null
