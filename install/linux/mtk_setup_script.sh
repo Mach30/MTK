a34207887ba2acce80320edc3f575de68b510c12
jwright <jwright@4549f004-a2ad-11e4-8197-c1af43012850>
2015-08-03 20:17:59 +0000

    First version of the Anaconda install script.
    git-svn-id: https://opendesignengine.net/svn/mtk/trunk@24 4549f004-a2ad-11e4-8197-c1af43012850

--- install/linux/mtk_setup_script.sh
+++ install/linux/mtk_setup_script.sh
@@ -1,65 +1,39 @@
 #!/usr/bin/env bash
 # Author: Mach 30: Foundation for Space Development - http://mach30.org
 # License: Apache 2.0
-
 # Writen and tested on Ubuntu 14.04
 # This script will ask for a sudo password
-# IPython setup based on http://bikulov.org/blog/2014/05/14/install-up-to-date-scientific-environment-in-ubuntu-14-dot-04-with-python-3-dot-4/
 
 echo "Installing the Mach 30 Mathematics Tool Kit (MTK)."
 echo "This can take awhile. Please be patient."
 
-echo "Running an initial update..."
-apt-get update > /dev/null
+echo "Switching to Downloads directory."
+cd ~/Downloads
 
-# Gain superuser status
-sudo
+echo "Downloading Anaconda installer."
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.3.0-Linux-x86_64.sh
 
-# Install python development packages and g++
-apt-get install -y python3-dev g++
+echo "Installing Anaconda. Please follow the instructions."
+bash Anaconda3-2.3.0-Linux-x86_64.sh
 
-# Install dependencies for scipy
-apt-get install -y libblas-dev liblapack-dev gfortran
+echo "Reloading bashrc to pick up Anaconda changes"
+source ~/.bashrc
 
-# Install dependencies for matplotlib
-apt-get install -y libfreetype6-dev libpng-dev
-
-# Drop super user status
-sudo -K
-
-# IPython notebook has bug:
-# https://bugs.launchpad.net/ubuntu/+source/python3.4/+bug/1290847
-python3 -m venv --clear --without-pip venv/ipython-notebook
-
-# Activate virtual env and install pip
-#source venv/ipython-notebook/bin/activate
-
-# Set up pip
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-rm get-pip.py
-
-# Install IPython
-pip3 install "ipython[all]"
-
-# Install scientific packages
-pip install numpy sympy matplotlib scipy pandas
-
-# Install ipython notebook and dependencies
-pip install pyzmq jinja2 pygments bokeh jsonschema
-
-# TODO: Determine if we need this
-# Install latest dev scikit-learn and build it
-#pip install cython https://github.com/scikit-learn/scikit-learn/archive/master.zip
-
-# Install prettyplotlib by Olga Botvinnik for beauty plots
-pip install brewer2mpl prettyplotlib
+echo "Updating Anaconda and installing IPython."
+bash -c "conda update conda"
+bash -c "conda update ipython ipython-notebook ipython-qtconsole"
 
 # Install Mach 30 dependencies
 pip install pint
 
-# Deactivate virtual env
-#deactivate
+cd ~/.ipython/nbextensions
+wget https://github.com/ipython-contrib/IPython-notebook-extensions/archive/3.x.zip
+unzip 3.x.zip
+rm unzip 3.x.zip
+
+# echo "Installing pandoc for PDF export."
+sudo apt-get install pandoc texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended 
 
 echo "Finished installing MTK. Enjoy."
 echo "If you have questions or want to get involved in the MTK project, see https://opendesignengine.net/projects/mtk"
+
