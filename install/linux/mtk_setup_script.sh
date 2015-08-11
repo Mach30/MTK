a90faef3f3352128b0fd6bd61c98614eeda8bae3
jwright <jwright@4549f004-a2ad-11e4-8197-c1af43012850>
2015-08-11 04:24:59 +0000

    First pass at changing Anaconda to being installed in /opt along with the IPython config files.
    git-svn-id: https://opendesignengine.net/svn/mtk/trunk@30 4549f004-a2ad-11e4-8197-c1af43012850

--- install/linux/mtk_setup_script.sh
+++ install/linux/mtk_setup_script.sh
@@ -14,26 +14,30 @@ echo "Downloading Anaconda installer."
 wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.3.0-Linux-x86_64.sh
 
 echo "Installing Anaconda. Please follow the instructions."
-bash Anaconda3-2.3.0-Linux-x86_64.sh -b -p /opt
+sudo bash Anaconda3-2.3.0-Linux-x86_64.sh -b -p /opt/anaconda3
 
 echo "Reloading bashrc to pick up Anaconda changes"
 source ~/.bashrc
 
 echo "Updating Anaconda and installing IPython."
-/opt/anaconda3/bin/conda update conda
-/opt/anaconda3/bin/conda update ipython ipython-notebook ipython-qtconsole
+sudo /opt/anaconda3/bin/conda update conda
+sudo /opt/anaconda3/bin/conda update ipython ipython-notebook ipython-qtconsole
+
+# Make sure user can launch IPython without specifying the full path
+sudo ln -s /opt/anaconda3/bin/ipython /usr/local/bin/iptyhon
 
 echo "Installing Mach 30 dependencies"
-/opt/anaconda3/bin/pip install pint
+sudo -H /opt/anaconda3/bin/pip install --upgrade pint
 
 echo "Initializing IPython configuration directories."
-/opt/anaconda3/bin/ipython --ipython-dir=/opt/ipython profile create
+export IPYTHONDIR="/opt/ipython"
+sudo /opt/anaconda3/bin/ipython profile create
 
 echo "Downloading and installing IPython notebook extensions"
 cd /opt/ipython/nbextensions
-wget https://github.com/ipython-contrib/IPython-notebook-extensions/archive/3.x.zip
-unzip 3.x.zip
-rm 3.x.zip
+sudo wget https://github.com/ipython-contrib/IPython-notebook-extensions/archive/3.x.zip
+sudo unzip 3.x.zip
+sudo rm 3.x.zip
 
 echo "Installing pandoc and some dependencies for PDF export."
 sudo apt-get install pandoc texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended 
